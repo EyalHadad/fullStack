19c85e3bd7d6c7cafe929853068bba6423561326
EyalHadad <eyalhadad1989@gmail>
2020-04-15 23:32:47 +0300

    first fullstack commit

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const postsRoutes = require("./routes/posts");
+const app = express();
+
+mongoose.connect("mongodb+srv://Eyal:88f33NP9XcdOCP9e@eyalscluster-edmrz.mongodb.net/node-angular?retryWrites=true&w=majority", { useNewUrlParser: true,useUnifiedTopology: true })
+  .then( () => {
+      console.log("Connected to database!");
+    })
+  .catch( () => {
+    console.log("Connected to failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use("/images", express.static(path.join("backend/images")));
+
+
+app.use((req,res,next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.use("/api/posts", postsRoutes);
+
+module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule ,CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
+import {PostCreateComponent} from "./posts/posts-create/post-create.component";
+import {HeaderComponent} from "./header/header.component";
+import {PostListComponent} from "./posts/post-list/post-list.component";
+import {HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from "./app-routing.module";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    PostListComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    ReactiveFormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
